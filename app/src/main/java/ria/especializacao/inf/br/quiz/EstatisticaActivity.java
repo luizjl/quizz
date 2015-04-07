7d71a1eb1afb05d33eb51fb844f7561f3b7eacf3
Luiz José de Lemos <luizlemos@gmail.com>
2015-04-07 03:35:42 -0300

    Grafico

--- app/src/main/java/ria/especializacao/inf/br/quiz/EstatisticaActivity.java
+++ app/src/main/java/ria/especializacao/inf/br/quiz/EstatisticaActivity.java
@@ -1,6 +1,7 @@
 package ria.especializacao.inf.br.quiz;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -12,7 +13,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -71,6 +74,7 @@ public class EstatisticaActivity extends ActionBarActivity {
         private TextView msgErros;
         private TextView msgTotal;
         private  View rootView;
+        RelativeLayout linearChart;
 
 
         public PlaceholderFragment() {
@@ -89,16 +93,56 @@ public class EstatisticaActivity extends ActionBarActivity {
 
             sessaoDAO = new SessaoDAO(getActivity());
 
-            Log.v("***Acertos",+sessaoDAO.getSessoes().getQtdAcertos()+" Erros:"+sessaoDAO.getSessoes().getQtdErros());
+            //Log.v("***Acertos",+sessaoDAO.getSessoes().getQtdAcertos()+" Erros:"+sessaoDAO.getSessoes().getQtdErros());
 
 
             msgAcertos.setText(msgAcertos.getText()+String.valueOf(sessaoDAO.getSessoes().getQtdAcertos()));
             msgErros.setText(msgErros.getText()+String.valueOf(sessaoDAO.getSessoes().getQtdErros()));
             msgTotal.setText(msgTotal.getText()+String.valueOf(sessaoDAO.getSessoes().getQtdAcertos()+sessaoDAO.getSessoes().getQtdErros()));
 
+            linearChart = (RelativeLayout) rootView.findViewById(R.id.linearChart);
+
+            int opcoes[] = {sessaoDAO.getSessoes().getQtdAcertos(), sessaoDAO.getSessoes().getQtdErros()};
+
+
+                drawChartAcertos(sessaoDAO.getSessoes().getQtdAcertos());
+                drawChartErros(sessaoDAO.getSessoes().getQtdErros());
 
 
             return rootView;
         }
+
+        public void drawChartAcertos( int acertos) {
+
+                View view = new View(getActivity());
+
+                view.setBackgroundColor(Color.GREEN);
+
+                view.setLayoutParams(new RelativeLayout.LayoutParams(100, acertos*10));
+
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) view
+                        .getLayoutParams();
+                params.setMargins(3, 0, 0, 0); // substitute parameters for left,
+                // top, right, bottom
+                view.setLayoutParams(params);
+                linearChart.addView(view);
+        }
+
+        public void drawChartErros( int erros) {
+
+            View view = new View(getActivity());
+
+            view.setBackgroundColor(Color.RED);
+
+            view.setLayoutParams(new RelativeLayout.LayoutParams(100, erros * 10));
+
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) view
+                    .getLayoutParams();
+            params.setMargins(200, 0, 0, 0); // substitute parameters for left,
+            // top, right, bottom
+            view.setLayoutParams(params);
+            linearChart.addView(view);
+        }
+
     }
 }
